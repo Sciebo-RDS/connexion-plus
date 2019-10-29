8d3b7f17f0bcbcec876997126d25b7c0da1c3e0b
Peter Heiss <peter.heiss@exchange.wwu.de>
2019-10-29 08:45:30 +0100

    publish package to pypi

--- connexion_plus/__init__.py
+++ connexion_plus/__init__.py
@@ -1,1 +1,3 @@
+name = "connexion-plus"
+
 from .Factory import Factory
--- setup.py
+++ setup.py
@@ -1,27 +1,34 @@
-from distutils.core import setup
+#from distutils.core import setup
+from setuptools import setup
+
+with open('README.md', encoding='utf-8') as f:
+    readme = f.read()
 
 setup(
-  name = 'connexion-plus',         # How you named your package folder (MyLib)
-  packages = ['connexion-plus'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'Connexion with benefits for microservices',   # Give a short description about your library
-  author = 'Peter Heiss',                   # Type in your name
-  author_email = 'peter.heiss@uni-muenster.de',      # Type in your E-Mail
-  url = 'https://github.com/Heiss',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/Heiss/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['connexion', 'microservice', 'tracing', 'prometheus', 'jaeger'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
+  name = 'connexion-plus',      
+  packages = ['connexion_plus'], 
+  version = '0.3',
+  license='MIT', 
+  description = 'Connexion with benefits for microservices',
+  long_description=readme,
+  long_description_content_type='text/markdown',
+  author = 'Peter Heiss',
+  author_email = 'peter.heiss@uni-muenster.de',
+  url = 'https://github.com/Heiss/connexion-plus',
+  download_url = 'https://github.com/Heiss/connexion-plus/archive/0.1.tar.gz',
+  keywords = ['connexion', 'microservice', 'tracing', 'prometheus', 'jaeger'],
+  python_requires='>=3.6',
+  install_requires=[    
           'connexion',
           'jaeger-client',
           'prometheus-flask-exporter'
       ],
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',      
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
