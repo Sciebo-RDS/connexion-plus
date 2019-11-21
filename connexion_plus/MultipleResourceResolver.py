4ef264fcebfb57e7b12ad3155a9ede4f0fbc14f1
Peter Heiss <peter.heiss@exchange.wwu.de>
2019-11-21 11:13:12 +0100

    Resources are now also resolved, when you use folders.

--- connexion_plus/MultipleResourceResolver.py
+++ connexion_plus/MultipleResourceResolver.py
@@ -1,7 +1,9 @@
-import re, string
+import re, string, logging
 import sys
 from connexion.resolver import RestyResolver, Resolution
 from connexion.exceptions import ResolverError
+
+logger = logging.getLogger('connexion.resolver')
 
 class MultipleResourceResolver(RestyResolver):
     def resolve(self, operation):
@@ -15,11 +17,45 @@ class MultipleResourceResolver(RestyResolver):
         #   /res1 [GET] and /res1/{id} [GET]
         operation._randomize_endpoint = 2
 
-        operation_id = self.resolve_operation_id(operation)
+        operation_id = self.resolve_operation_id(operation)    
+
+        def try_resolve(*args):
+            size = len(args)
+
+            i = 0
+            errors = []
+            for op in args:
+                # check, if the user uses files for resources Title()
+                try:
+                    return Resolution(self.resolve_function_from_operation_id(op), op)
+                except ResolverError as e:
+                    logger.error(e)
+                    errors.append(e)
+
+                # check, if the user uses files for resources lower()
+                try:
+                    return Resolution(self.resolve_function_from_operation_id(op.lower()), op)
+                except ResolverError as e:
+                    logger.error(e)
+                    errors.append(e)
+
+            raise errors[-1]
+
+        # remove router controller first, so it does not merge in the imported file in the following replacement
+        # remove all dots
+        operation_id_file = operation_id.replace(f"{self.default_module_name}.", "")
+        c = operation_id_file.count(".")
+        operation_id_file = operation_id_file.replace(".", "", c - 1)
+        # add router controller again
+        operation_id_file = f"{self.default_module_name}.{operation_id_file}"
+
         try:
-            return Resolution(self.resolve_function_from_operation_id(operation_id), operation_id)
-        except ResolverError:
-            return Resolution(self.resolve_function_from_operation_id(operation_id.lower()), operation_id)
+            # try to import functions in resource folders (e.g. /Res1/{Para1}/Res2 resolves in Res1.Res2)
+            # try to import functions in resource files (e.g. /Res1/{Para1}/Res2 resolves in Res1Res2)
+            return try_resolve(operation_id, operation_id_file)
+        except:
+            raise
+        
             
 
     def resolve_operation_id_using_rest_semantics(self, operation):
@@ -47,16 +83,20 @@ class MultipleResourceResolver(RestyResolver):
             resource_name = ''
             # split path at slash to separate every parameter
             split = re.split("/", operation.path)
+            
+            resources = []
+            
             for s in split:
                 # find the parameter, where a variable was defined to exlude it in resource_name
                 pattern = re.compile(r"\{[a-zA-Z-_]+\}")
                 if s:
                     if pattern.search(s) is None:
                         count_resource += 1 # count up the founded resource
-                        resource_name += s.title()
+                        resources.append(s.title())
                     else:
                         count_parameters += 1
 
+            resource_name = ".".join(resources)
             if x_router_controller:
                 name = x_router_controller
 
