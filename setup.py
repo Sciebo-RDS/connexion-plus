cc1ecdcde8e2fcb3c44f1961282d68deb2ced52a
Peter Heiss <peter.heiss@uni-muenster.de>
2020-02-17 13:55:52 +0100

    move github actions

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', encoding='utf-8') as f:
 setup(
   name = 'connexion-plus',      
   packages = ['connexion_plus'], 
-  version = '0.37',
+  version = '0.36',
   license='MIT', 
   description = 'Connexion with benefits for microservices',
   long_description=readme,
