81e01647cb01211e8f533cba6f96bc476301ffe7
Peter Heiss <peter.heiss@uni-muenster.de>
2020-10-21 10:54:33 +0200

    tracing ignores now /metrics

--- connexion_plus/Application.py
+++ connexion_plus/Application.py
@@ -6,7 +6,8 @@ from connexion import FlaskApp
 
 class App(FlaskApp):
     def __init__(
-        self, name,
+        self,
+        name,
         use_tracer=None,
         use_metric=False,
         use_logging_level=logging.DEBUG,
@@ -15,7 +16,8 @@ class App(FlaskApp):
         use_default_error=None,
         use_scheduler=None,
         all=None,
-        *args, **kwargs
+        *args,
+        **kwargs,
     ):
         # TODO: Add more text here for current situation
         """
@@ -57,7 +59,8 @@ class App(FlaskApp):
 
         if not isinstance(self.app, (Flask, FlaskApp)):
             logger.warning(
-                "Given App is not flask, so it cannot get any functionality added from this lib currently.")
+                "Given App is not flask, so it cannot get any functionality added from this lib currently."
+            )
             return
 
         # add default error
@@ -73,6 +76,7 @@ class App(FlaskApp):
                 logger.info("use given handler.")
 
             else:
+
                 def handle_error(e):
                     code = 500
                     if isinstance(e, HTTPException):
@@ -81,7 +85,7 @@ class App(FlaskApp):
                     error = {
                         "error": e.__class__.__name__,
                         "http_code": code,
-                        "description": str(e)
+                        "description": str(e),
                     }
                     logger.exception(error)
                     return jsonify(error), code
@@ -91,8 +95,7 @@ class App(FlaskApp):
                 logger.info("use default one")
 
             # register for all json exceptions
-            self.app.register_error_handler(
-                Exception, self.default_errorhandler)
+            self.app.register_error_handler(Exception, self.default_errorhandler)
 
             # register handler for all http exceptions
             for ex in default_exceptions:
@@ -141,6 +144,7 @@ class App(FlaskApp):
             # TODO: add configuration https://github.com/rycus86/prometheus_flask_exporter#configuration
 
             from prometheus_flask_exporter import PrometheusMetrics
+
             self.metrics = PrometheusMetrics(self.app)
             logger.info("Add prometheus to Flask")
 
@@ -150,6 +154,20 @@ class App(FlaskApp):
             # add tracing to all routes in flaskApp
             from flask_opentracing import FlaskTracing
             import opentracing
+            from functools import wraps
+            from flask import request
+
+            def wrapper(fn):
+                @wraps(fn)
+                def request_func(*args, **kwargs):
+                    if request.path != "/metrics":
+                        return fn(*args, **kwargs)
+
+            FlaskTracing._before_request_fn = wrapper(FlaskTracing._before_request_fn)
+            FlaskTracing._after_request_fn = wrapper(FlaskTracing._after_request_fn)
+            FlaskTracing.end_trace_with_error = wrapper(
+                FlaskTracing.end_trace_with_error
+            )
 
             config = None
             if not isinstance(use_tracer, opentracing.Tracer):
@@ -157,15 +175,12 @@ class App(FlaskApp):
                 from jaeger_client import Config as jConfig
 
                 tracer_config = {
-                    'sampler': {
-                        'type': 'const',
-                        'param': 1,
+                    "sampler": {"type": "const", "param": 1,},
+                    "local_agent": {
+                        "reporting_host": "jaeger-agent",
+                        "reporting_port": 5775,
                     },
-                    'local_agent': {
-                        'reporting_host': "jaeger-agent",
-                        'reporting_port': 5775,
-                    },
-                    'logging': True,
+                    "logging": True,
                 }
 
                 if isinstance(use_tracer, dict):
@@ -173,18 +188,21 @@ class App(FlaskApp):
 
                 if isinstance(use_metric, bool) and use_metric is True:
                     logger.info("Use metrics for tracer.")
-                    from jaeger_client.metrics.prometheus import PrometheusMetricsFactory
+                    from jaeger_client.metrics.prometheus import (
+                        PrometheusMetricsFactory,
+                    )
+
                     config = jConfig(
                         config=tracer_config,
                         service_name=f"{name}ConnexionPlus",
                         metrics_factory=PrometheusMetricsFactory(
-                            namespace=f"{name}ConnexionPlus")
+                            namespace=f"{name}ConnexionPlus"
+                        ),
                     )
                 else:
                     logger.info("no metrics for tracer configured.")
                     config = jConfig(
-                        config=tracer_config,
-                        service_name=f"{name}ConnexionPlus",
+                        config=tracer_config, service_name=f"{name}ConnexionPlus",
                     )
             else:
                 logger.info("use given tracer config.")
@@ -194,6 +212,7 @@ class App(FlaskApp):
 
             # add tracer to everything to support spans through multiple microservices via rpc-calls
             from opentracing_instrumentation.client_hooks import install_all_patches
+
             install_all_patches()
             logger.info("All tracing relevant libs patched.")
 
@@ -203,7 +222,7 @@ class App(FlaskApp):
             th = TracingHandler(use_tracer)
             th.setLevel(use_logging_level)
 
-            logging.getLogger('').addHandler(th)
+            logging.getLogger("").addHandler(th)
             logger.info("Finished Tracer adding.")
 
         logger.info("--- Finished Connexion-Plus ---")
